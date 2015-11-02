929ddcf9a3d94ecfb27bb8b70d58e95eea1db1c9
Henry Rodrick <moshisushi@gmail.com>
2015-11-02 03:54:13 +0000

    Set base_url to http://saga-test and mention in Docker Guide.txt

--- Site/application/config/config.php
+++ Site/application/config/config.php
@@ -12,7 +12,7 @@
  * If this is not set then CodeIgniter will guess the protocol, domain and path
  * to your installation.
  */
-$config['base_url']	= '/';
+$config['base_url']	= 'http://saga-test/';
 
 /*
  *------------------------------------------------------------------------------
