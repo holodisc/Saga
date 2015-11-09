f72cc577a11382e4ddd1d516ac8f9b88e715a167
holodisc <holodiscent@gmail.com>
2015-11-09 01:56:00 +0000

    Add option to set docker port 80 forward port

--- run-docker.sh
+++ run-docker.sh
@@ -2,6 +2,36 @@
 
 set -eu
 
+FORWARD_PORT=80
+
+
+function print_help {
+    echo "Usage: $0 [-h] [-p port]"
+    printf "  -h\t%s\n" "this help"
+    printf "  -p\t%s\n" "host OS port to forward the container's port 80 to (default: 80)"
+}
+
+while getopts ":hp:" opt; do
+    case $opt in
+        p)
+            FORWARD_PORT=$OPTARG
+            echo "Forwarding container port 80 to port $FORWARD_PORT"
+            ;;
+        h)
+            print_help
+            exit 1
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
+
 docker build --tag saga .
 
-docker run -p 80:80 -h 'saga-test' -it saga /saga/Docker/setup.sh
+docker run -p $FORWARD_PORT:80 -h 'saga-test' -it saga /saga/Docker/setup.sh
